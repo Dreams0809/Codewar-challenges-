97e2bcf8f1329898d07d8bba65b182aac21cdac9
Dreams0809 <104542625+Dreams0809@users.noreply.github.com>
2022-09-22 23:34:35 -0400

    Add files via upload

---
+++ Testing1-2-3.js
@@ -0,0 +1,10 @@
+﻿// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Solution
+
+var number=function(array){
+    let i = 1
+    return array.map( str => `${i++}: ${str}`)
+}
