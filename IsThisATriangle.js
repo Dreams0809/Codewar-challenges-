d10cc4569f13a1dfa74a4e9754fa06845741bca2
Dreams0809 <104542625+Dreams0809@users.noreply.github.com>
2022-10-14 18:36:16 -0400

    Add files via upload

---
+++ IsThisATriangle.js
@@ -0,0 +1,14 @@
+﻿// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// Solution
+
+
+function isTriangle(a,b,c){
+    if( a+b >c && a+c>b && b+c>a){
+    
+      return true;}
+    
+    else{
+     
+        return false;}
+}
