a8f2a47c873b484b7797f1107c6690574a84efeb
Dreams0809 <104542625+Dreams0809@users.noreply.github.com>
2022-10-16 11:36:43 -0400

    Add files via upload

---
+++ sentenceSmash.js
@@ -0,0 +1,19 @@
+﻿// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Solution
+
+function smash (words) {
+    let sentence = ""
+    
+    if(words.length === 0){
+      return sentence
+    
+    } else if (words.length === 1){
+        return words.toString()
+    
+    }else{
+       sentence += words.join(" ")
+    } 
+    
+      return sentence
+}
